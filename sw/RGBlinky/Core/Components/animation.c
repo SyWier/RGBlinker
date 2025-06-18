974b1a1fe1156a2698108b2ba8a7d3aa12e49905
sdani <email.sinko@gmail.com>
2025-06-18 23:09:38 +0200

    Added shift instruction

--- sw/RGBlinky/Core/Components/animation.c
+++ sw/RGBlinky/Core/Components/animation.c
@@ -77,9 +77,9 @@ void Animation_Prev() {
 }
 
 const LedFrame_t frames1[] = {
-		{load, { RGB(255, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0),RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0) } },
-		{load, { RGB(0, 0, 0), RGB(0, 255, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0),RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0) } },
-		{load, { RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 255), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0),RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0) } },
+		{load, { RGB(255, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0),RGB(0, 0, 0), RGB(0, 0, 0), RGB(0, 0, 0) } },
+		{repeat, { 35 } },
+		{shift, {1} },
 };
 
 const LedFrame_t frames2[] = {
@@ -101,9 +101,9 @@ const LedFrame_t frames3[] = {
 #define LEN(var) ( sizeof(var)/sizeof(var[0]) )
 
 LedAnimation_t animations[] = {
-		{frames3, LEN(frames3), 100},
-		{frames1, LEN(frames1), 250},
+		{frames1, LEN(frames1), 50},
 		{frames2, LEN(frames2), 500},
+		{frames3, LEN(frames3), 100},
 };
 
 LedAnimator_t Animator = {
@@ -127,7 +127,7 @@ void load(const uint8_t* data) {
 }
 void add(const uint8_t* data) {
 	for(int i = 0; i < LED_CNT; i++) {
-		Animator.pwmBuffer[i] += data[i];
+		Animator.pwmBuffer[i] += (int8_t)data[i];
 	}
 }
 
@@ -136,7 +136,43 @@ void repeat(const uint8_t* data) {
 }
 
 void shift(const uint8_t* data) {
-	Log_Debug("(Instr) shift: %d", data[0]);
+	int8_t val = (int8_t)data[0];
+
+	if(val > 0) {
+		Log_Debug("(Instr) shift right: %d", data[0]);
+
+		uint8_t temp[val];
+
+		// Copy last k elements to temp
+		memcpy(temp, &Animator.pwmBuffer[LED_CNT - val], val);
+
+		// Shift the rest to the right
+		memmove(&Animator.pwmBuffer[val], Animator.pwmBuffer, LED_CNT - val);
+
+		// Copy temp to the front
+		memcpy(Animator.pwmBuffer, temp, val);
+
+		return;
+	}
+
+	if(val < 0) {
+		Log_Debug("(Instr) shift left: %d", data[0]);
+
+		val = -val;
+
+		uint8_t temp[val];
+
+		// Copy first k elements to temp
+		memcpy(temp, Animator.pwmBuffer, val);
+
+		// Shift the rest to the left
+		memmove(Animator.pwmBuffer, &Animator.pwmBuffer[val], LED_CNT - val);
+
+		// Copy temp to the end
+		memcpy(&Animator.pwmBuffer[LED_CNT - val], temp, val);
+
+		return;
+	}
 }
 
 void Animate() {
